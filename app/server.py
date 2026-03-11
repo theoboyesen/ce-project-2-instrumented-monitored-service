621650d95dc4f1fbb12ae78219d98a7dd5f260c5
Theo <tboyesen12@hotmail.com>
2026-03-11 12:33:46 +0000

    Added latency metric to python app

--- app/server.py
+++ app/server.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 import uuid
-
 import logging
 import json
 from datetime import datetime
-from flask import g
+import time
 
+# Logging setup
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger()
@@ -17,6 +17,7 @@ formatter = logging.Formatter('%(message)s')
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
+
 
 def log_json(level, event, **kwargs):
     log_entry = {
@@ -35,23 +36,46 @@ def log_json(level, event, **kwargs):
         logger.error(json.dumps(log_entry))
 
 
-
+# Create Flask app
 app = Flask(__name__)
 
+
+# Assign request ID
 @app.before_request
 def assign_request_id():
     g.request_id = str(uuid.uuid4())
 
+
+# Start latency timer
+@app.before_request
+def start_timer():
+    g.start_time = time.time()
+
+
+# Log request latency
+@app.after_request
+def log_request_latency(response):
+    duration_ms = (time.time() - g.start_time) * 1000
+
+    log_json(
+        "INFO",
+        "request_completed",
+        path=request.path,
+        latency_ms=duration_ms
+    )
+
+    return response
+
+
 # In-memory order storage
 orders = {}
 
-# Health check endpoint
+
 @app.route("/health")
 def health():
     return jsonify({"status": "healthy"}), 200
 
 
-# Create an order
 @app.route("/orders", methods=["POST"])
 def create_order():
     data = request.get_json()
@@ -67,18 +91,16 @@ def create_order():
     orders[order_id] = order
 
     log_json(
-    "INFO",
-    "order_created",
-    order_id=order_id,
-    item=order["item"],
-    price=order["price"]
-)
-
+        "INFO",
+        "order_created",
+        order_id=order_id,
+        item=order["item"],
+        price=order["price"]
+    )
 
     return jsonify(order), 201
-    
 
-# Get an order
+
 @app.route("/orders/<order_id>", methods=["GET"])
 def get_order(order_id):
 
@@ -99,7 +121,6 @@ def get_order(order_id):
     )
 
     return jsonify(order), 200
-
 
 
 if __name__ == "__main__":
